91acbe2c37cfc7528ded65a092123a4ee55208cd
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-14 03:59:22 +0530

    first commit
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

---
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.dailycodebuffer.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+}
