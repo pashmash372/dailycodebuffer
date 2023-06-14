ecef690b611145b31ebd230097b16fcd42cd6bdf
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-14 16:12:09 +0530

    data filtering using @JsonIgnore
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/model/Employee.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/model/Employee.java
@@ -1,15 +1,19 @@
 package com.dailycodebuffer.springbootdemo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties({"department"})
 public class Employee {
 
     private String employeeId;
     private String firstName;
     private String lastName;
     private String emailId;
+//    @JsonIgnore
     private String department;
 }
