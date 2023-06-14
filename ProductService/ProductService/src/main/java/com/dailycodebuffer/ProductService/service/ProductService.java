bd23c593b281bab33b296672a10c62c92c5d8ed0
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-14 20:04:38 +0530

    implementing Create Product API
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductService.java
+++ ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductService.java
@@ -1,4 +1,7 @@
 package com.dailycodebuffer.ProductService.service;
 
+import com.dailycodebuffer.ProductService.model.ProductRequest;
+
 public interface ProductService {
+    long addProduct(ProductRequest productRequest);
 }
--- ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
+++ ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
@@ -1,7 +1,29 @@
 package com.dailycodebuffer.ProductService.service;
 
+import com.dailycodebuffer.ProductService.entity.Product;
+import com.dailycodebuffer.ProductService.model.ProductRequest;
+import com.dailycodebuffer.ProductService.repository.ProductRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class ProductServiceImpl implements  ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public long addProduct(ProductRequest productRequest) {
+        log.info("Adding Product..");
+        Product product = Product.builder()
+                .productName(productRequest.getName())
+                .price(productRequest.getPrice())
+                .quantity(productRequest.getQuantity())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product Created");
+        return product.getProductId();
+    }
 }
