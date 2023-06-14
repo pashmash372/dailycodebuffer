09b431bf651222ab7bfccc6a3a2eb98461db464b
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-14 20:56:43 +0530

    implementing Get Product API
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
+++ ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
@@ -2,17 +2,21 @@ package com.dailycodebuffer.ProductService.service;
 
 import com.dailycodebuffer.ProductService.entity.Product;
 import com.dailycodebuffer.ProductService.model.ProductRequest;
+import com.dailycodebuffer.ProductService.model.ProductResponse;
 import com.dailycodebuffer.ProductService.repository.ProductRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.BeanUtils.copyProperties;
+
 @Service
 @Log4j2
-public class ProductServiceImpl implements  ProductService {
+public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
     @Override
     public long addProduct(ProductRequest productRequest) {
         log.info("Adding Product..");
@@ -25,5 +29,19 @@ public class ProductServiceImpl implements  ProductService {
         productRepository.save(product);
         log.info("Product Created");
         return product.getProductId();
+    }
+
+    @Override
+    public ProductResponse getProductById(long productId) {
+        log.info("Get the product for productId: {}", productId);
+        Product product =
+                productRepository.findById(productId)
+                        .orElseThrow(
+                                () -> new RuntimeException("Product with given id not found"));
+        ProductResponse productResponse
+                = new ProductResponse();
+        copyProperties(product, productResponse);
+
+        return productResponse;
     }
 }
