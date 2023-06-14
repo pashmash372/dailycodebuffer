b585126bcc646d2a38714b2aa9574fa7238bed1f
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-14 17:18:55 +0530

    get employee data from db from jpa
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/controller/EmployeeV2Controller.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/controller/EmployeeV2Controller.java
@@ -4,10 +4,9 @@ import com.dailycodebuffer.springbootdemo.model.Employee;
 import com.dailycodebuffer.springbootdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v2/employees")
@@ -22,5 +21,23 @@ public class EmployeeV2Controller {
     public Employee save(@RequestBody Employee employee) {
         return employeeService.save(employee);
     }
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable("id") String id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployeeById(@PathVariable("id") String id) {
+        return employeeService.deleteEmployeeById(id);
+    }
+
 
 }
--- spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/service/EmployeeV2ServiceImpl.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/service/EmployeeV2ServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeV2ServiceImpl implements  EmployeeService{
@@ -29,12 +30,27 @@ public class EmployeeV2ServiceImpl implements  EmployeeService{
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<EmployeeEntity> employeeEntityList
+                = employeeRepository.findAll();
+
+        List<Employee> employeeList = employeeEntityList
+                .stream()
+                .map(entity -> {
+                    Employee employee = new Employee();
+                    BeanUtils.copyProperties(entity, employee);
+                    return employee;
+                })
+                .collect(Collectors.toList());
+        return employeeList;
     }
 
     @Override
     public Employee getEmployeeById(String id) {
-        return null;
+        EmployeeEntity employeeEntity =
+                employeeRepository.findById(id).get();
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeEntity, employee);
+        return employee;
     }
 
     @Override
