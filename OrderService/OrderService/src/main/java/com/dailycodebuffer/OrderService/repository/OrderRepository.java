6a8c43577823280b1c1cb58fca0dc6af94754334
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-14 22:11:19 +0530

    Creating Order Service Application with packages and Classes
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

---
+++ OrderService/OrderService/src/main/java/com/dailycodebuffer/OrderService/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.dailycodebuffer.OrderService.repository;
+
+import com.dailycodebuffer.OrderService.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends
+        JpaRepository<Order, Long> {
+}
