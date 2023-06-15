84001d84c24d301a1c73045edd7a3be7ee36398e
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-15 20:54:32 +0530

    Adding Security in Product Service
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/controller/ProductController.java
+++ ProductService/ProductService/src/main/java/com/dailycodebuffer/ProductService/controller/ProductController.java
@@ -6,37 +6,38 @@ import com.dailycodebuffer.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
     private ProductService productService;
 
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
-    public ResponseEntity<Long>addProduct
-            (@RequestBody ProductRequest productRequest){
-        long productId = productService
-                .addProduct(productRequest);
+    public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
+        long productId = productService.addProduct(productRequest);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer') || hasAuthority('SCOPE_internal')")
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse>getProductById(
-            @PathVariable("id") long productId){
-        int a = 0;
-        ProductResponse productResponse = productService
-                .getProductById(productId);
-        return  new ResponseEntity<>(productResponse, HttpStatus.OK);
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
+        ProductResponse productResponse
+                = productService.getProductById(productId);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
-
 
     @PutMapping("/reduceQuantity/{id}")
     public ResponseEntity<Void> reduceQuantity(
             @PathVariable("id") long productId,
-            @RequestParam("quantity") int quantity){
-        productService.reduceQuantity(productId, quantity);
+            @RequestParam long quantity
+    ) {
+        productService.reduceQuantity(productId, (int) quantity);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
