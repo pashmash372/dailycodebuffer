b99736a130766cc3941208b0c1ebc4c0f75b5e48
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-15 22:24:41 +0530

    Create a Test Class for Service layer
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

---
+++ OrderService/OrderService/src/test/java/com/dailycodebuffer/OrderService/service/OrderServiceImplTest.java
@@ -0,0 +1,41 @@
+package com.dailycodebuffer.OrderService.service;
+
+import com.dailycodebuffer.OrderService.external.client.PaymentService;
+import com.dailycodebuffer.OrderService.external.client.ProductService;
+import com.dailycodebuffer.OrderService.repository.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class OrderServiceImplTest {
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private PaymentService paymentService;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    OrderService orderService = new OrderServiceImpl();
+
+    @DisplayName("Get Order - Success Scenario")
+    @Test
+    void test_When_Order_Successfully() {
+        // Mocking
+        // Actual
+        orderService.getOrderDetails(1L);
+        // Verification
+        // Assert
+    }
+}
