d63ebecdef61cc4b81347340cc150071d584c18f
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-15 14:10:30 +0530

    Implementing GetOrder Details in Order Service
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- OrderService/OrderService/src/main/java/com/dailycodebuffer/OrderService/service/OrderServiceImpl.java
+++ OrderService/OrderService/src/main/java/com/dailycodebuffer/OrderService/service/OrderServiceImpl.java
@@ -1,10 +1,12 @@
 package com.dailycodebuffer.OrderService.service;
 
 import com.dailycodebuffer.OrderService.entity.Order;
+import com.dailycodebuffer.OrderService.exception.CustomException;
 import com.dailycodebuffer.OrderService.external.client.PaymentService;
 import com.dailycodebuffer.OrderService.external.client.ProductService;
 import com.dailycodebuffer.OrderService.external.request.PaymentRequest;
 import com.dailycodebuffer.OrderService.model.OrderRequest;
+import com.dailycodebuffer.OrderService.model.OrderResponse;
 import com.dailycodebuffer.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,5 +75,26 @@ public class OrderServiceImpl implements  OrderService{
 
         log.info("Order Places successfully with Order Id: {}", order.getId());
         return order.getId();
+    }
+
+    @Override
+    public OrderResponse getOrderDetails(long orderId) {
+        log.info("Get order details for Order Id: {}", orderId);
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(()-> new CustomException(
+                        "Order not found for Id: "+ orderId,
+                        "NOT_FOUND",
+                        404
+                ));
+        OrderResponse orderResponse
+                = OrderResponse.builder()
+                .orderId(order.getId())
+                .orderStatus(order.getOrderStatus())
+                .amount(order.getAmount())
+                .orderDate(order.getOrderDate())
+                .build();
+
+        return orderResponse;
     }
 }
