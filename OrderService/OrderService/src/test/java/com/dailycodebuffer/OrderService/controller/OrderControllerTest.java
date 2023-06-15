4d10cd626376ea932b638b800a9f9a39591672c7
pashmash372 <abhilashsivasanker@gmail.com>
2023-06-16 00:43:36 +0530

    Adding Dependencies in Controller Test
    Signed-off-by: pashmash372 <abhilashsivasanker@gmail.com>

--- OrderService/OrderService/src/test/java/com/dailycodebuffer/OrderService/controller/OrderControllerTest.java
+++ OrderService/OrderService/src/test/java/com/dailycodebuffer/OrderService/controller/OrderControllerTest.java
@@ -3,7 +3,13 @@ package com.dailycodebuffer.OrderService.controller;
 import com.dailycodebuffer.OrderService.OrderServiceConfig;
 import com.dailycodebuffer.OrderService.repository.OrderRepository;
 import com.dailycodebuffer.OrderService.service.OrderService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,13 +17,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest({"server.port=0"})
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = {OrderServiceConfig.class})
 public class OrderControllerTest {
 
+    @Autowired
+    private OrderService orderService;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer
+            = WireMockExtension.newInstance()
+            .options(WireMockConfiguration
+                    .wireMockConfig()
+                    .port(8080))
+            .build();
+
+    private ObjectMapper objectMapper
+            = new ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 }
